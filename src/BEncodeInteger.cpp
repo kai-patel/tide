53d2e70453c355d08af7944e35772afcaf40193a
Kai Patel <00kaipatel@gmail.com>
2023-07-20 21:52:49 +0100

    refactor: integer parser to use attributes

--- src/BEncodeInteger.cpp
+++ src/BEncodeInteger.cpp
@@ -1,28 +1,37 @@
 #include <BEncodeInteger.hpp>
+#include <boost/fusion/adapted.hpp>
 #include <boost/phoenix/core.hpp>
 #include <boost/phoenix/operator.hpp>
 #include <boost/spirit/include/qi.hpp>
 
+BOOST_FUSION_ADAPT_STRUCT(tide::BEncodeInteger, (long, number));
+
 namespace tide {
 
+template <typename I>
+struct integer_parser : ::boost::spirit::qi::grammar<I, tide::BEncodeInteger> {
+  ::boost::spirit::qi::rule<I, tide::BEncodeInteger> start;
+
+  integer_parser() : integer_parser::base_type(start) {
+    using ::boost::phoenix::ref;
+    using ::boost::spirit::qi::_1;
+    using ::boost::spirit::qi::long_;
+    start %= ('i' >> long_ >> 'e');
+  }
+};
+
 bool BEncodeInteger::parse(std::string input) {
-  using boost::phoenix::ref;
-  using boost::spirit::qi::_1;
-  using boost::spirit::qi::long_;
-  using boost::spirit::qi::phrase_parse;
-  using boost::spirit::qi::ascii::space;
+  auto first = input.cbegin();
+  auto last = input.cend();
 
-  auto first = input.begin();
-  auto last = input.end();
+  bool r = boost::spirit::qi::parse(
+      first, last, integer_parser<std::string::const_iterator>(), *this);
 
-  bool result = phrase_parse(
-      first, last, ('i' >> long_[ref(this->number) = _1] >> 'e'), space);
-
-  if (result == false || first != last) {
+  if (r == false || first != last) {
     return false;
   }
 
-  return result;
+  return true;
 }
 
 } // namespace tide
