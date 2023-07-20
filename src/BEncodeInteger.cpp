837d327854eb4f0ed22a605861a37cf81e57cf1b
Kai Patel <00kaipatel@gmail.com>
2023-07-20 20:22:52 +0100

    feat: bencode integer parsing

--- src/BEncodeInteger.cpp
+++ src/BEncodeInteger.cpp
@@ -1,7 +1,28 @@
 #include <BEncodeInteger.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
+#include <boost/spirit/include/qi.hpp>
 
 namespace tide {
 
-void BEncodeInteger::parse(std::string input) { this->number = 42; }
+bool BEncodeInteger::parse(std::string input) {
+  using boost::phoenix::ref;
+  using boost::spirit::qi::_1;
+  using boost::spirit::qi::long_;
+  using boost::spirit::qi::phrase_parse;
+  using boost::spirit::qi::ascii::space;
+
+  auto first = input.begin();
+  auto last = input.end();
+
+  bool result = phrase_parse(
+      first, last, ('i' >> long_[ref(this->number) = _1] >> 'e'), space);
+
+  if (result == false || first != last) {
+    return false;
+  }
+
+  return result;
+}
 
 } // namespace tide
