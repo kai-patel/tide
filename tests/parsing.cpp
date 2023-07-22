0c2aba6b77a838fcba930b093806e0d7e4873163
Kai Patel <00kaipatel@gmail.com>
2023-07-22 11:42:43 +0100

    test: test for parsing lists of lists

--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -1,3 +1,4 @@
+#include "BEncodeValue.hpp"
 #include <boost/any.hpp>
 #include <gtest/gtest.h>
 #include <tide.hpp>
@@ -35,4 +36,24 @@ TEST(Parsing, List) {
 
   EXPECT_EQ("spam", std::get<tide::BEncodeString>(actual[0]));
   EXPECT_EQ(42, std::get<tide::BEncodeInteger>(actual[1]));
+}
+
+TEST(Parsing, ListofLists) {
+  std::string input = "l4:spaml3:hami42eei101ee";
+
+  tide::BEncodeValue result;
+
+  bool is_parsed = result.parse(input);
+
+  auto actual = std::get<tide::BEncodeList>(result.value).contents;
+
+  EXPECT_EQ("spam", std::get<tide::BEncodeString>(actual[0]));
+
+  auto inner_list =
+      std::get<boost::recursive_wrapper<tide::BEncodeList>>(actual[1]);
+
+  EXPECT_EQ("ham", std::get<tide::BEncodeString>(inner_list.get().contents[0]));
+  EXPECT_EQ(42, std::get<tide::BEncodeInteger>(inner_list.get().contents[1]));
+
+  EXPECT_EQ(101, std::get<tide::BEncodeInteger>(actual[2]));
 }
