07a1990bf6891b9e3de61b0404bfbd9a16b4fe71
Kai Patel <00kaipatel@gmail.com>
2023-07-24 16:04:36 +0100

    docs: add doxygen target

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['metainfoexception_0',['MetaInfoException',['../classtide_1_1_meta_info_exception.html',1,'tide']]]
+];
---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dict_5fparser_0',['dict_parser',['../structtide_1_1dict__parser.html',1,'tide']]]
+];
